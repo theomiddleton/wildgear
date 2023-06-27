5e2ff9e665684346a6f052e177e2dd6e5ce830a5
theo <theo.middleton@outlook.com>
2023-06-27 12:59:13 +0100

    updated subscription tiers

--- src/config/subscriptions.ts
+++ src/config/subscriptions.ts
@@ -2,17 +2,17 @@ import { env } from "@/env.mjs"
 import type { SubscriptionPlan } from "@/types"
 
 export const freePlan: SubscriptionPlan = {
-  name: "Ollie",
+  name: "Denali",
   description:
-    "The ollie plan allows you to create up to 1 store, and 20 products.",
+    "The Denali plan allows you to create up to 1 store, and 20 products.",
   stripePriceId: "",
   monthlyPrice: null,
 }
 
 export const proPlan: SubscriptionPlan = {
-  name: "Kickflip",
+  name: "Everest",
   description:
-    "The kickflip plan allows you to create up to 3 stores, and 20 products per store.",
+    "The Everest plan allows you to create up to 3 stores, and 20 products per store.",
   stripePriceId: env.STRIPE_PRO_STORE_PRICE_ID ?? "",
   monthlyPrice: 10,
 }
